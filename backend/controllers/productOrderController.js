5f3b4cf1f1b882454cee979f1e40bad82328ec84
Alexis-Lu <alexis.lu.dev@gmail.com>
2023-11-05 21:06:43 +0100

    feat: add CRUD functions for product in order

---
+++ backend/controllers/productOrderController.js
@@ -0,0 +1,69 @@
+const e = require("express");
+const connection = require("../configs/db.js");
+
+exports.findAll = (req, res) => {
+  connection.query(
+    "SELECT * FROM ProductOrder",
+    function (error, results, fields) {
+      if (error) throw error;
+      res.send(results);
+    }
+  );
+};
+
+exports.findOne = (req, res) => {
+  connection.query(
+    "SELECT * FROM ProductOrder WHERE id = ?",
+    [req.params.id],
+    function (error, results) {
+      if (error) throw error;
+      res.send(results);
+    }
+  );
+};
+
+exports.findAllByOrder = (req, res) => {
+  connection.query(
+    "SELECT * FROM ProductOrder WHERE orderId = ?",
+    [req.params.id],
+    function (error, results) {
+      if (error) throw error;
+      res.send(results);
+    }
+  );
+};
+
+exports.update = (req, res) => {
+  const { idProduct, quantity, price } = req.body;
+  connection.query(
+    "UPDATE ProductOrder SET quantity = ?, price = ? WHERE id = ? AND idProduct = ?",
+    [quantity, price, req.params.id, idProduct],
+    function (error, results) {
+      if (error) throw error;
+      res.send(results);
+    }
+  );
+};
+
+exports.create = (req, res) => {
+  const { idOrder, idProduct, quantity, price } = req.body;
+  connection.query(
+    "INSERT INTO ProductOrder (orderId, productId, quantity, price) VALUES (?, ? ,? ,?)",
+    [idOrder, idProduct, quantity, price],
+    function (error, results) {
+      if (error) throw error;
+      res.send(results);
+    }
+  );
+};
+
+exports.delete = (req, res) => {
+  connection.query(
+    "DELETE FROM ProductOrder WHERE id = ?",
+    [req.params.id],
+    function (error, results) {
+      if (error) throw error;
+      res.send(results);
+    }
+  );
+};
