d12abb4a8e7cf6f862c0a27fba809426be6db559
Alexis-Lu <alexis.lu.dev@gmail.com>
2023-11-05 21:33:17 +0100

    feat: use all routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
 var express = require("express");
 var mysql = require("mysql");
+var cors = require("cors");
 var app = express();
+app.use(cors());
+
 app.use(express.json());
-const router = require("./routes/products.js");
-app.listen(3001, () => {});
-app.use(router);
+
+const productsRoutes = require("./routes/products.js");
+const userRoutes = require("./routes/users.js");
+const productOrderRoutes = require("./routes/productOrder.js");
+const orderRoutes = require("./routes/order.js");
+app.use(userRoutes, productsRoutes, productOrderRoutes, orderRoutes);
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
