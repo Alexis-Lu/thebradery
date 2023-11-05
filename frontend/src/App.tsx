4f90fe1647c12d44bffeaba92d74933babbe7a2f
Alexis-Lu <alexis.lu.dev@gmail.com>
2023-11-05 21:42:52 +0100

    feat: add home page with header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,16 @@ import logo from "./logo.svg"
 import { Counter } from "./features/counter/Counter"
 import "./App.css"
 import React from "react"
+import Header from "./components/header"
+import Home from "./pages/home"
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  )
 }
 
 export default App
