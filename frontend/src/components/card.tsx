c64993ccba57a49cae52e0bd3229886f9c0346d4
Alexis-Lu <alexis.lu.dev@gmail.com>
2023-11-11 02:41:20 +0100

    fix: fixing card and header component

--- frontend/src/components/card.tsx
+++ frontend/src/components/card.tsx
@@ -1,21 +1,90 @@
 import React from "react"
-import { Card, CardContent, CardMedia, Typography } from "@mui/material/"
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  Button,
+} from "@mui/material/"
+import AddCardOutlinedIcon from "@mui/icons-material/AddCardOutlined"
+import { addProductToCart } from "../features/cart/cartSlice"
+import { useDispatch, useSelector } from "react-redux"
+import { toast } from "react-toastify"
 
 interface Props {
+  id: number
   name: string
   price: string
+  inventory: number
+  img: string
 }
 
 export default function ProductCard(props: Props) {
+  const dispatch = useDispatch()
+  const cart = useSelector((state: any) => state.cart.items)
+
+  function handleAddToCart(datas: any) {
+    dispatch(addProductToCart(datas))
+  }
+
   return (
-    <Card>
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="h2">
-          {props.name}
-        </Typography>
-        <Typography variant="body2" color="textSecondary" component="p">
-          {props.price}
-        </Typography>
+    <Card
+      sx={{
+        boxShadow: "0 0 0 0 transparent",
+      }}
+    >
+      <CardContent
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <CardMedia
+          component="img"
+          height="300"
+          image={props.img}
+          alt="random"
+        ></CardMedia>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "10px",
+            width: "100%",
+          }}
+        >
+          <Typography gutterBottom variant="h5" component="h1">
+            {props.name}
+          </Typography>
+          <Typography
+            variant="body2"
+            component="p"
+            style={{
+              fontSize: "20px",
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              width: "100%",
+            }}
+          >
+            <div>{props.price}€ </div>
+            <div>
+              {props.inventory === 0 ? (
+                "Stock épuisé"
+              ) : (
+                <Button
+                  style={{ border: "1px solid" }}
+                  onClick={() => handleAddToCart(props)}
+                >
+                  <AddCardOutlinedIcon />
+                </Button>
+              )}
+            </div>
+          </Typography>
+        </div>
       </CardContent>
     </Card>
   )
--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,11 +1,29 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import "../styles/components/header.css"
 import logo from "../public/the-bradery-logo_v2.svg"
-import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined"
-import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined"
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined"
+import TemporaryDrawer from "./drawer"
+import PopoverComponent from "./popover"
+
+interface userInfos {
+  firstname: string
+  lastname: string
+  email: string
+  password: string
+  address: string
+}
 
 export default function Header() {
+  const [userInfos, setUserInfos] = useState<userInfos[] | null>(null)
+  useEffect(() => {
+    const userInfosString = localStorage.getItem("userInfos")
+    console.log(userInfosString)
+    if (userInfosString) {
+      const userDatas = JSON.parse(userInfosString)
+      setUserInfos(userDatas)
+    }
+  }, [])
+
   return (
     <div className="container">
       <div className="containerLeft">
@@ -13,16 +31,29 @@ export default function Header() {
         // TODO: finir search <input type="text" placeholder="Search" />
       </div>
       <div className="containerCenter">
-        <img className="logo" src={logo} alt="The Bradery" />
+        <img
+          className="logo"
+          src={logo}
+          alt="The Bradery"
+          onClick={() => (window.location.href = "/")}
+          style={{ cursor: "pointer" }}
+        />
       </div>
 
       <div className="containerRight">
-        <div className="containerRightItems">// TODO - add Name</div>
-        <div className="containerRightItems">
-          <AccountCircleOutlinedIcon />
+        <div
+          className="containerRightItems"
+          style={{ fontSize: "22px", fontWeight: "bold" }}
+        >
+          {userInfos
+            ? `${userInfos[0].firstname} ${userInfos[0].lastname}`
+            : null}
         </div>
         <div className="containerRightItems">
-          <ShoppingBagOutlinedIcon />
+          <PopoverComponent />
+        </div>
+        <div className="containerRightItems">
+          <TemporaryDrawer />
         </div>
       </div>
     </div>
