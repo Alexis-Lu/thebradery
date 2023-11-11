2899ef6b7ccfd716ba7f284133a14d6ea24b0412
Alexis-Lu <alexis.lu.dev@gmail.com>
2023-11-11 02:31:44 +0100

    feat: adding toaster and more routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,27 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
 import { store } from "./app/store"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import App from "./App"
 import "./index.css"
+import Login from "./pages/login"
+import Register from "./pages/register"
+import Commands from "./pages/commands"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <div>
+    <ToastContainer position="top-center" />
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/commands" element={<Commands />} />
+        </Routes>
+      </Router>
+    </Provider>
+  </div>,
 )
