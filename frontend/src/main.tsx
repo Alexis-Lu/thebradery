bfef597fbdb9b0474216a53c594bed0bcee4043e
Alexis-Lu <alexis.lu.dev@gmail.com>
2023-11-12 00:02:44 +0100

    feat: add product informations route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,6 +10,7 @@ import Register from "./pages/register"
 import Commands from "./pages/commands"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
+import Product from "./pages/product"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <div>
@@ -21,6 +22,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/commands" element={<Commands />} />
+          <Route path="/product/:id" element={<Product />} />
         </Routes>
       </Router>
     </Provider>
